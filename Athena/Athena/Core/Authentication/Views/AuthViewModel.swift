6cfeef2bb0860a43e70fab07eba55a6758f765bc
Tshimega Belmont <tshimega@athena-io.com>
2022-09-26 15:06:48 +0200

    Initialising AuthViewModel

---
+++ Athena/Athena/Core/Authentication/Views/AuthViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewModel.swift
+//  Athena
+//
+//  Created by Tshimega Belmont on 2022/09/26.
+//
+
+import SwiftUI
+//import Firebase
+
+class AuthViewModel: ObservableObject {
+//    @Published var userSession: FirebaseAuth.User?
+    
+    
+    init() {
+//        self.userSession = Auth.auth().currentUser
+    }
+}
