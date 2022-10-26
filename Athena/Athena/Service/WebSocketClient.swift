b0e2c6c609811c5a35ba2ad51075e2548c213316
DevJaco <dev2708dev@gmail.com>
2022-10-26 14:52:04 +0200

    Dev: Implementing websockets in a cleaner manner

--- Athena/Athena/Service/WebSocketClient.swift
+++ Athena/Athena/Service/WebSocketClient.swift
@@ -3,46 +3,49 @@ import Foundation
 
 
 final class WebsocketClient: NSObject {
-        
-    override init() {
-        
-        if let data = KeychainHelper.standard.read(service: "access-token"){
-            let accessToken = String(data: data, encoding: .utf8)
-            print("DEBUG: AccessToken for WebSocket is \(String(describing: accessToken))")
-        }
-        else {
-            print("DEBUG: No webSocket access token found in keychain")
-        }
-    }
     
-    
-    
-    static let shared = WebsocketClient()
     var webSocket: URLSessionWebSocketTask?
-    
     var opened = false
+    let accessToken: String
+    static let shared = WebsocketClient()
     
     private var urlString = "ws://localhost:8000"
     
+    private override init() {
+        if let data = KeychainHelper.standard.read(service: "access-token"){
+            if let accessToken = String(data: data, encoding: .utf8){
+                self.accessToken = accessToken
+            }
+            else {
+                self.accessToken = ""
+                print("DEBUG: Something went wrong setting access token string value")
+            }
+        }
+        else {
+            self.accessToken = ""
+            print("DEBUG: No webSocket access token found in keychain")
+        }
+    }
     
     func openWebSocket() {
         if let url = URL(string: urlString) {
             var request = URLRequest(url: url)
             
-            // The value here is an access token from my local backend, needs to be replaced with value from keychain. 
-            request.addValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3NTc2MTcyLCJpYXQiOjE2NjY3MTIxNzIsImp0aSI6ImJjYjNmNmY0YWQ1NzRmYjQ4OTE2ODY1YzQ0YmZiMmI0IiwidXNlcl9pZCI6Ijc1YmIxZmEwLTEwNjMtNDZiNC04M2IzLTk3NjI2M2YyNDk1MCJ9.4e3oQwWuIT6rxk1wBfDSFnsSVYrO_GV-otZUne6YVug", forHTTPHeaderField: "Authorization")
+            print("DEBUG: AccessToken for WebSocket is \(String(describing: accessToken))")
+            request.addValue(accessToken, forHTTPHeaderField: "Authorization")
             let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
             let webSocket = session.webSocketTask(with: request)
             self.webSocket = webSocket
-            self.opened = true
+            opened = true
             self.webSocket?.resume()
-        } else {
+        }
+        else {
             webSocket = nil
         }
     }
     
     func closeWebSocket() {
-        print("DEBUG: Handle closing websocket..")
+        self.webSocket!.cancel(with: .goingAway, reason: nil)
     }
     
 }
@@ -52,7 +55,7 @@ extension WebsocketClient: URLSessionWebSocketDelegate {
         print("DEBUG: Opened websocket connection")
         opened = true
     }
-
+    
     
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
         print("DEBUG: Closed websocket connection.")
