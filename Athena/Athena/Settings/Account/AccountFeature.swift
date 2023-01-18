336f220d3dccee12b24456214d9e0ad318aab682
Tshimega Belmont <tshimegabelmont@gmail.com>
2023-01-18 15:47:03 +0200

    Added Account Feature

---
+++ Athena/Athena/Settings/Account/AccountFeature.swift
@@ -0,0 +1,71 @@
+//
+//  AccountFeature.swift
+//  Athena
+//
+//  Created by Tshimega Belmont on 2022/12/12.
+//
+
+import Foundation
+import SwiftUI
+import ComposableArchitecture
+
+struct AccountFeature: ReducerProtocol {
+        
+    struct State: Equatable {
+        var route: Route = .idle
+    }
+    
+    enum Route: Equatable {
+        case idle
+    }
+    
+    enum Action: Equatable {
+        case openAccountManagement
+        case updateNotificationSettings
+        case openPrivacySettings
+        case updateFamily
+        case setHomeAddress
+        case updateAccount
+        case signOut
+        case dismissView
+    }
+    
+    @EnvironmentObject var userAuth: AuthViewModel
+
+    
+    var body: some ReducerProtocol<State, Action>{
+        
+        
+        Reduce { state, action in
+            
+            switch action {
+                
+            case .updateAccount:
+                return .none
+                
+            case .setHomeAddress:
+                return .none
+                
+            case .updateFamily:
+                return .none
+                
+            case .openPrivacySettings:
+                return .none
+                
+            case .updateNotificationSettings:
+                return .none
+                
+            case .openAccountManagement:
+                return .none
+                
+            case .signOut:
+                print("DEBUG: Handle Sign Out User")
+                return .none
+                
+            case .dismissView:
+                return .none
+            }
+            
+        }
+    }
+}
