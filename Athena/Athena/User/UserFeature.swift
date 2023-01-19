493013333151ed7114ed47a93687351e12023af3
DevJaco <dev2708dev@gmail.com>
2023-01-19 15:26:54 +0200

    Removing user environment and reducer from UserFeature - This is an old version of composable, go look at the ones we have done previously. Did you use chatgpt?

--- Athena/Athena/User/UserFeature.swift
+++ Athena/Athena/User/UserFeature.swift
@@ -1,15 +1,109 @@
 //
-//  UserFeature.swift
+//  AuthViewModel.swift
 //  Athena
 //
-//  Created by Tshimega Belmont on 2022/11/11.
+//  Created by Tshimega Belmont on 2022/09/26.
 //
 
-import ComposableArchitecture
 import Foundation
+import Combine
+import SwiftUI
 
-struct UserState: Equatable {
-  var user: User?
+class AuthViewModel: ObservableObject {
+    var didChange = PassthroughSubject<AuthViewModel, Never>()
+    
+    //@AppStorage("Authentication_Status") var isAuthenticated = (!username.isEmpty() || !username.isExpired() ) ? true : false - where accessToken is from the keychain
+    
+    @AppStorage("Access_Token") var isAuthenticated = false {
+        willSet { objectWillChange.send() }
+    }
+    
+    @AppStorage("USER_KEY") var username = ""
+    @Published var password = ""
+    @Published var email = ""
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var cellPhone = ""
+    
+    
+    @Published var invalid: Bool = false
+    @Published var alreadyExists: Bool = false
+    @Published var selectedIndex = 2
+    
+    init() {
+        
+        if let data = KeychainHelper.standard.read(service: "access-token"){
+            let accessToken = String(data: data, encoding: .utf8)
+            print("DEBUG: Current username is \(username)")
+            print("DEBUG: Current access token is \(String(describing: accessToken))")
+            self.isAuthenticated = true
+        }
+        else {
+            print("DEBUG: No access token found in keychain")
+            self.isAuthenticated = false
+        }
+    }
+    
+    
+    func loginCitizen() {
+        Webservice().login(username: username, password: password) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let accessToken):
+                    print("DEBUG: Access token is \(accessToken)")
+                    let data = Data(accessToken.utf8)
+                    KeychainHelper.standard.save(data, service: "access-token")
+                    
+                    self.isAuthenticated = true
+                    self.password = ""
+                    
+                case .failure(let error):
+                    self.invalid = true
+                    print("DEBUG: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func registerCitizen() {
+        Webservice().register(username: username, email: email, password: password) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let accessToken):
+                    print("DEBUG: Access token is \(accessToken)")
+                    let data = Data(accessToken.utf8)
+                    KeychainHelper.standard.save(data, service: "access-token")
+                    
+                    self.isAuthenticated = true
+                    self.password = ""
+                    
+                case .failure(let error):
+                    self.alreadyExists = true
+                    print("DEBUG: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func updateCitizen(withFirstName firstname: String, lastName: String, cellNumber: String) {
+        print("DEBUG: User Firstname is \(firstname), Lastname is \(lastName), and Phone number is \(cellNumber).")
+    }
+    
+    func logoutCitizen() {
+        print("DEBUG: Logged out the current user...")
+        KeychainHelper.standard.delete(service: "access-token")
+        
+        DispatchQueue.main.async {
+            self.isAuthenticated = false
+            self.selectedIndex = 2
+        }
+        
+    }
+    
+    func fetchCitizen() {
+        print("DEBUG: Handle fetch the logged in user's data...")
+    }
+    
 }
 
 enum UserAction: Equatable {
@@ -17,28 +111,4 @@ enum UserAction: Equatable {
   case dataLoaded(Result<User, APIError>)
 }
 
-struct UsersEnvironment {
-  var userRequest: (JSONDecoder) -> Effect<User, APIError>
-}
 
-//let userReducer = Reducer<
-//  UserState,
-//  UserAction,
-//  SystemEnvironment<UserEnvironment>
-//> { state, action, environment in
-//  switch action {
-//  case .onAppear:
-//    return environment.userRequest(environment.decoder())
-//      .receive(on: environment.mainQueue())
-//      .catchToEffect()
-//      .map(UserAction.dataLoaded)
-//  case .dataLoaded(let result):
-//    switch result {
-//    case .success(let user):
-//      state.user = user
-//    case .failure:
-//      break
-//    }
-//    return .none
-//  }
-//}
